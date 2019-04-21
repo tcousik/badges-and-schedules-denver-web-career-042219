7c2eaefd7cb4bf655d0b9993e6d770c2bcad4c2b
Tejas Cousik <tcousik0@gmail.com>
2019-04-21 05:09:06 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,3 +9,20 @@ def batch_badge_creator(array)
   end
   badge_list
 end
+
+def assign_rooms(speakers)
+    rooms = []
+    speakers.each_with_index do|name, index|
+    rooms << "Hello, #{name}! You'll be assigned to room #{index + 1}!"
+    end
+rooms
+end
+
+def printer(speakers)
+    batch_badge_creator(speakers).each do |name| 
+        puts name
+    end 
+    assign_rooms(speakers).each do |room| 
+        puts room
+    end  
+end
